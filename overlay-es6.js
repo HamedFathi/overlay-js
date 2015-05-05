aefaa69dd82e1e95af07767ea89cf7a38742b729
Pieter Beulque <pieterbeulque@gmail.com>
2015-05-05 13:49:45 +0200

    Updated documentation

--- overlay-es6.js
+++ overlay-es6.js
@@ -1,5 +1,8 @@
-class Overlay {
+export default class Overlay {
 
+  // @constructor
+  // @param {HTMLElement} el
+  // @param {Object}      options
   constructor (el = document.getElementsByClassName('js-overlay')[0], options = {}) {
     this.el = el;
 
@@ -20,15 +23,12 @@ class Overlay {
     this.init().bind();
   }
 
+  // Initial setup
   init () {
     return this;
   }
 
-  /**
-   * Bind events
-   *
-   * @return {Overlay}
-   */
+  // Bind event handlers to certain DOM elements, specified through
   bind () {
     this.bindDelegate('click', this.options.selectors.show, e => {
       e.preventDefault();
@@ -51,28 +51,36 @@ class Overlay {
     return this;
   }
 
+  // Helper method to support clicks on elements that don't exist yet
+  // @param {String}    evt       Event to listen for
+  // @param {String}    selector  Element selector that triggers the event
+  // @param {Function}  handler   Callback method
   bindDelegate (evt, selector, handler) {
     let matches = document.body.matches || document.body.webkitMatchesSelector || document.body.msMatchesSelector;
 
+    // Listen to all events of type evt
     document.body.addEventListener(evt, e => {
+      // If e.target matches the specified selector or is a child element
       if (!!matches.call(e.target, `${selector}, ${selector} *`)) {
+        // Execute handler, pass on the Event object
         handler(e);
       }
     }, false);
   }
 
+  // Show the overlay
   show () {
     let regex = new RegExp("\\s*" + this.options.states.hidden + "\\s*", 'gi'); // Template string not working because of reasons
-    console.log('[Overlay] Show');
     this.el.className = this.el.className.replace(regex, '') + ' ' + this.options.states.shown;
   }
 
+  // Hide the overlay
   hide () {
     let regex = new RegExp("\\s*" + this.options.states.shown + "\\s*", 'gi'); // Template string not working because of reasons
-    console.log('[Overlay] Hide');
     this.el.className = this.el.className.replace(regex, '') + ' ' + this.options.states.hidden;
   }
 
+  // Toggle the overlay
   toggle () {
     if (this.el.className.indexOf(this.options.states.shown) === -1) {
       this.show();
@@ -81,9 +89,8 @@ class Overlay {
     }
   }
 
+  // Render HTML inside the content selector
   render (html) {
     this.el.querySelector(this.options.selectors.content).innerHTML = html;
   }
 }
-
-export default Overlay;
